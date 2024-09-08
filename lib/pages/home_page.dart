4cdb5578b00c041d59794edeb7d335245127f815
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2024-09-09 02:53:11 +0530

    Refactor HomePage to include custom bottom navigation bar and drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,54 @@
+import "package:basic_ecommerce_app/components/bottom_nav_bar.dart";
+import "package:basic_ecommerce_app/components/my_drawer.dart";
+import "package:basic_ecommerce_app/pages/cart_page.dart";
+import "package:basic_ecommerce_app/pages/shop_page.dart";
 import "package:flutter/material.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
+      ),
+      drawer: const MyDrawer(),
+      body: _pages[_selectedIndex],
+    );
   }
 }
